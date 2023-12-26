5207491614dcf88861bc5e2312322e5189159b27
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2023-12-26 23:08:06 +0100

    productpageidslugaddition

---
+++ app/product/[id]/page.js
@@ -0,0 +1,25 @@
+"use client"
+
+import MainLayout from "@/app/layouts/MainLayout"
+
+
+export default function Product({ params }) {
+
+        return (
+            <>
+            
+                <MainLayout>
+                    <div className="max-w-width[1200px] mx-auto">
+
+                    </div>
+                
+
+                </MainLayout>
+            
+            
+            
+            
+            </>
+        )
+
+}
