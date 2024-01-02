ca675ce2fad3005d7936886802e5a967eb967d41
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2024-01-02 17:05:39 +0100

    productpageupdate

---
+++ app/components/SimilarProducts.js
@@ -0,0 +1,73 @@
+"use client"
+
+import ProductComp from './Product';
+import {BiLoader} from 'react-icons/bi';
+
+
+export default function SimilarProducts(){
+
+    const products = [
+
+        {
+          id:1, 
+          title:"brown leather bag", 
+          description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer.",
+          url:"https://picsum.photos/id/2",
+          price: 250
+        },
+        {
+          id:2, 
+          title:"bycle veyroy", 
+          description: "Lorem Ipsum is sbycuile to rtide in sidewalks yuou know nd typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer.",
+          url:"https://picsum.photos/id/67",
+          price: 1990
+        },
+        {
+          id:3, 
+          title:"random item", 
+          description: "Lorem Ipsum is sbycuile to rtide in sidewalks yuofdkgjhfdkhgdikjdfdfdgnsbfgjhsu know nd typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer.",
+          url:"https://picsum.photos/id/10",
+          price: 177
+        },
+    
+    
+      ],
+
+    return (
+        <>
+        
+        
+            <div>
+                
+                <div className="border-b py-1 max-w-[1200px] mx-auto"/>
+
+                <div className="max-w-[1200px] mx-auto">
+                    <div className="font-bold text-2xl py-2 mt-4">Similar sponsored items</div>
+
+                    {products.length > 0 ?
+                            <div className="grid grid-cols-5 gap-4">
+                                {products.map(product => (
+
+                                        <ProductComp key={product.id} product={product} />
+
+                                ))}    
+                            </div>
+                        : <div className='flex items-center justify-center'>
+                            <div className='flex items-center justify-center gap-4 font-semibold' >
+                                <BiLoader size={30} className='text-blue-400 animate-spin'/> 
+                                Loading Products...
+                            </div>
+                        </div>}
+                    
+                </div>
+
+
+
+            </div>
+        
+        
+
+        </>
+       
+    )
+}
--- app/product/[id]/page.js
+++ app/product/[id]/page.js
@@ -1,99 +1,82 @@
-"use client"
+"use client";
 
-import MainLayout from "@/app/layouts/MainLayout"
-
+import MainLayout from "@/app/layouts/MainLayout";
+import SimilarProducts from '@/app/components/SimilarProducts';
 
 export default function Product({ params }) {
+  const product = {
+    id: 1,
+    title: "brown leather bag",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer.",
+    url: "https://picsum.photos/id/2",
+    price: 250,
+  };
 
-        
-    const product = {
+  return (
+    <>
+      <MainLayout>
+        <div className="max-w-width[1200px] mx-auto">
+          <div className="flex px-4 py-10">
+            {product?.url ? (
+              <img className="w-[40%] rounded-lg" src={product?.url + "/280"} />
+            ) : (
+              <div className="w-[40%] rounded-lg"></div>
+            )}
 
-        
-          id:1, 
-          title:"brown leather bag", 
-          description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer.",
-          url:"https://picsum.photos/id/2",
-          price: 250
-        
-    
-     }
-    
-    
-    
-    return (
-            <>
-            
-                <MainLayout>
-                    <div className="max-w-width[1200px] mx-auto">
-                        <div className="flex px-4 py-10" >
+            <div className="px-4 w-full">
+              <div className="font-bold text-xl">{product?.title}</div>
+              <div className="text-gray-700 text-sm pt-2">Used on Occasion</div>
 
-                            { product?.url 
-                            ? <img className="w-[40%] rounded-lg" src={product?.url+'/280'} /> 
-                            : <div className="w-[40%] rounded-lg" ></div>  
-                            }
+              <div className="border-b py-1"></div>
 
-                        <div className="px-4 w-full" >
-                            <div className="font-bold text-xl">{product?.title}</div>
-                            <div className="text-gray-700 text-sm pt-2">Used on Occasion</div>
+              <div className="pt-3 pb-2">
+                <div className="flex items-center">
+                  Condition:{" "}
+                  <span className="font-bold text-[17px] ml-2">New</span>
+                </div>
+              </div>
 
-                            <div className="border-b py-1"></div>
+              <div className="border-b py-1"></div>
 
-                            <div className="pt-3 pb-2">
-                                <div className="flex items-center">
-                                    Condition: <span className="font-bold text-[17px] ml-2">New</span>
-                                </div>
-                            </div>
+              <div className="pt-3">
+                <div className="w-full flex items-center justify-between">
+                  <div className="flex items-center">
+                    Price:
+                    {product?.price ? (
+                      <div className="font-bold text-[17px] ml-2">
+                        EUR € {(product?.price / 100).toFixed(2)}
+                      </div>
+                    ) : null}
+                  </div>
 
-                            <div className="border-b py-1"></div>
+                  <button
+                    className=" bg-[#3498C9]
+                                     text-white py-2 px-20 rounded-full cursor-pointer"
+                  >
+                    Add to Cart
+                  </button>
+                </div>
+              </div>
 
-                            <div className="pt-3">
-                                <div className="w-full flex items-center justify-between">
-                                    <div className="flex items-center">
-                                        Price: 
-                                        {product?.price 
-                                        ? <div className="font-bold text-[17px] ml-2">
-                                            EUR € {(product?.price/ 100).toFixed(2)}
-                                        </div>
-                                        :  null }
-                                        
-                                    </div>
+              <div className="border-b py-1" />
 
-                                    
-                                    <button className=" bg-[#3498C9]
-                                     text-white py-2 px-20 rounded-full cursor-pointer">
-                                            Add to Cart
-                                    </button>
-                                </div>
-                            </div>
-
-                            <div className="border-b py-1" />
-                                    
-                                    <div className="pt-3">
-                                            <div className="font-semibold pb-1">
-                                                Description:
-                                            </div>
-                                            <div className=" text-gray-700 text-sm pt-2">{product?.description}</div>
-                                    </div>
+              <div className="pt-3">
+                <div className="font-semibold pb-1">Description:</div>
+                <div className=" text-gray-700 text-sm pt-2">
+                  {product?.description}
+                </div>
+              </div>
 
 
-                        </div>
 
+            </div>
 
-                            
+          </div>
 
-                        </div>
-
-
-                        </div>
-                    
-                
-
-                </MainLayout>
-            
-            
-            
-            
-            </>
-        )
-
+        </div>
+          <SimilarProducts />
+      </MainLayout>
+    </>
+  );
 }
