d6fa9ba15a8cb11433b578b3f5e2b34f3203905c
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2024-01-07 19:38:01 +0100

    checkoutint

---
+++ app/checkout/page.js
@@ -0,0 +1,30 @@
+"use client"
+
+import MainLayout from "../layouts/MainLayout"
+
+export default function Checkout(){
+
+
+    return (
+        <>
+                <MainLayout>
+                            <div id="CheckoutPage" className="mt-4 max-w-[1100px] mx-auto">
+
+
+                                <div className="text-2xl font-bold mt-4 mb-4">Checkout</div>
+
+                                <div className="relative flex items-baseline gap-4 justify-between mx-auto w-full">
+
+                                    <div  >
+
+                                    </div>
+
+                                </div>
+
+
+
+                            </div>    
+                </MainLayout>       
+        </>
+    )
+}
