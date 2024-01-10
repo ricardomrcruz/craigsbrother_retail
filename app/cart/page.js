aac6cafd19bf10160fb037e41867719f0a815e25
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2024-01-10 14:22:18 +0100

    checkitem

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,76 +1,61 @@
-"use client"
+"use client";
 
 import MainLayout from "../layouts/MainLayout";
 import SimilarProducts from "../components/SimilarProducts";
-import CartItem from '../components/CartItem';
-
+import CartItem from "../components/CartItem";
 
 export default function Cart() {
-
-    const product = {
-          id:1, 
-          title:"brown leather bag", 
-          description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer.",
-          url:"https://picsum.photos/id/3",
-          price: 250
-        }
-
-
+  const product = {
+    id: 1,
+    title: "brown leather bag",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer.",
+    url: "https://picsum.photos/id/3",
+    price: 250,
+  };
 
   return (
     <>
-
-
       <MainLayout>
-            <div className="max-w-[1200px] mx-auto mb-8 min-h-[300px]" >
-                <div className="text-2xl font-bold my-4">Shopping Cart</div>
-                <div className="relative flex items-baseline justify-between gap-2">
-                    <div className="w-[60%]">
-                            <CartItem key={product.id} product={product} />
-                    </div>
-
-                    <div id="GoToCheckout" className="md:w-[33%] absolute top-0 right-0 m-2">
-                        <div className="bg-white p-4 border">
-
-                          <button className="flex items-center justify-center bg-blue-600 w-full text-white font-semibold p-3 rounded-full mt-4">
-                              Go To Checkout
-                          </button>
-
-
-                          <div className="flex items-center justify-between mt-4 text-sm mb-2">
-                            <div>Items (3)</div>
-                            <div>€12.99</div>
-                          </div>
-                          
-                          <div className="flex items-center justify-between mb-4 text-sm ">
-                            <div>Shipping</div>
-                            <div>Free</div>
-                          </div>
-
-                          <div className="border-b border-gray-300"/>
-
-                          <div className="flex items-center justify-between mt-4 mb-1 text-lg font-semibold">
-                            <div className="">Subtotal</div>
-                            <div className="">€12.99</div>                        
-                          </div>
-
-                        </div>
-
-                    </div>
-
-
-
-
-
-                </div>
-
+        <div className="max-w-[1200px] mx-auto mb-8 min-h-[300px]">
+          <div className="text-2xl font-bold my-4">Shopping Cart</div>
+          <div className="relative flex items-baseline justify-between gap-2">
+            <div className="w-[60%]">
+              <CartItem key={product.id} product={product} />
             </div>
 
+            <div
+              id="GoToCheckout"
+              className="md:w-[33%] absolute top-0 right-0 m-2"
+            >
+              <div className="bg-white p-4 border">
+                <button className="flex items-center justify-center bg-blue-600 w-full text-white font-semibold p-3 rounded-full mt-4">
+                  Go To Checkout
+                </button>
 
+                <div className="flex items-center justify-between mt-4 text-sm mb-2">
+                  <div>Items (3)</div>
+                  <div>€12.99</div>
+                </div>
+
+                <div className="flex items-center justify-between mb-4 text-sm ">
+                  <div>Shipping</div>
+                  <div>Free</div>
+                </div>
+
+                <div className="border-b border-gray-300" />
+
+                <div className="flex items-center justify-between mt-4 mb-1 text-lg font-semibold">
+                  <div className="">Subtotal</div>
+                  <div className="">€12.99</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
 
         <SimilarProducts />
       </MainLayout>
-
     </>
-  )
+  );
 }
--- app/checkout/page.js
+++ app/checkout/page.js
@@ -1,30 +1,42 @@
-"use client"
+"use client";
 
-import MainLayout from "../layouts/MainLayout"
+import MainLayout from "../layouts/MainLayout";
+import CheckoutItem from "../components/CheckoutItem";
 
-export default function Checkout(){
-
-
-    return (
-        <>
-                <MainLayout>
-                            <div id="CheckoutPage" className="mt-4 max-w-[1100px] mx-auto">
+export default function Checkout() {
 
 
-                                <div className="text-2xl font-bold mt-4 mb-4">Checkout</div>
+  return (
+    <>
+      <MainLayout>
+        <div id="CheckoutPage" className="mt-4 max-w-[1100px] mx-auto">
+          <div className="text-2xl font-bold mt-4 mb-4">Checkout</div>
 
-                                <div className="relative flex items-baseline gap-4 justify-between mx-auto w-full">
+          <div className="relative flex items-baseline gap-4 justify-between mx-auto w-full">
+            <div className="w-[45%]">
+              <div className="bg-white rounded-lg p-4 border">
+                <div className="text-xl font-semibold mb-2">
+                  Shipping Address
+                </div>
+              </div>
 
-                                    <div  >
+              <div>
+                <ul className="text-sm mt-2">
+                  <li>Name: Test</li>
+                  <li>Address: Test</li>
+                  <li>Zipcode: Test</li>
+                  <li>City: Test</li>
+                  <li>Country: Test</li>
+                </ul>
+              </div>
 
-                                    </div>
-
-                                </div>
-
-
-
-                            </div>    
-                </MainLayout>       
-        </>
-    )
+              <div className="bg-white rounded-lg mt-4">
+                <CheckoutItem key={product.id} product={product} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </MainLayout>
+    </>
+  );
 }
