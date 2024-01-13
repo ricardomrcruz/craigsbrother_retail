fb564bafd20811e0036dd2500b3061d06632b3d1
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2024-01-13 13:36:08 +0100

    ckout

--- app/checkout/page.js
+++ app/checkout/page.js
@@ -16,11 +16,14 @@ export default function Checkout() {
   return (
     <>
       <MainLayout>
+        
         <div id="CheckoutPage" className="mt-4 max-w-[1100px] mx-auto">
+          
+          
           <div className="text-2xl font-bold mt-4 mb-4">Checkout</div>
 
           <div className="relative flex items-baseline gap-4 justify-between mx-auto w-full">
-            <div className="w-[45%]">
+            <div className="w-[65%]">
               <div className="bg-white rounded-lg p-4 border">
                 <div className="text-xl font-semibold mb-2">
                   Shipping Address
@@ -42,38 +45,57 @@ export default function Checkout() {
               </div>
             </div>
 
-            <div className="relative -top-[6px] w-[35%] border rounded-lg">
-                <div>Shipping:</div>
-                <div>Free</div>
-            </div>
-
-            <div className="border-t" />
-
-            <div className="flex items-center justify-between my-4" >
-                <div className="font-semibold"> Order total: </div>
-                <div className="text-2xl font-semibold">
-                      €12.99
+            <div
+              id="PlaceOrder"
+              className="relative -top-[6px] w-[35%] border rounded-lg"
+                >
+              <div className="p-4">
+                <div className="flex items-baseline justify-between text-sm mb-1">
+                  <div>Items (3)</div>
+                  <div>€14.99</div>
                 </div>
-            </div>
-
-            <form >
-                <div className="border border-gray-500 p-2 rounded-sm" id="card-element"/>
-                <p
-                    id="card-error"
-                    role="alert"
-                    className="text-red-700 text-center font-semibold relative top-2"
-                    >
-
-                </p>
-
-        
                 
-            </form>
+                <div className="flex items-baseline justify-between text-sm ">
+                  <div>Shipping:</div>
+                  <div>Free</div>
+                </div>
+                 
+                  <div className="border-t" />
+
+                  <div className="flex items-center justify-between my-4">
+                    <div className="font-semibold"> Order total: </div>
+                    <div className="text-2xl font-semibold">€14.99</div>
+                  </div>
+
+                                    <form>
+                                        <div
+                                        className="border border-gray-500 p-2 rounded-sm"
+                                        id="card-element"
+                                        />
+                                        <p
+                                        id="card-error"
+                                        role="alert"
+                                        className="text-red-700 text-center font-semibold relative top-2"
+                                        ></p>
+
+                                        <button
+                                        type="submit"
+                                        className="mt-4 bg-blue-600 text-lg w-full text-white font-semibold p-3 rounded-full"
+                                        >
+                                        Confirm and pay
+                                        </button>
+                                    </form>
+              </div>
 
 
 
+            </div>
           </div>
+
+
+
         </div>
+      
       </MainLayout>
     </>
   );
