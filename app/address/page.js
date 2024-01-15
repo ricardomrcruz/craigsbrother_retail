d8bada7519b1c63bd10d92ffbe321e90b4c98135
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2024-01-15 13:57:59 +0100

    addresspage

---
+++ app/address/page.js
@@ -0,0 +1,42 @@
+"use client"
+
+
+
+import MainLayout from "../layouts/MainLayout"
+import { AiOutlineLoading3Quarters } from "react-icons/ai"
+
+export default function Address() {
+
+
+    return (
+        <>
+            <MainLayout>
+
+                    <div id="AddressPage" className="mt-4 max-w-[600px] mx-auto px-2">
+
+                        <div className="mx-auto bg-white rounded-lg p-3">
+
+                            <div className="text-xl text-bold mb-2">
+                                Address Details:
+                            </div>
+
+                            <form>
+                                
+                                <div className="mb-4">
+                             
+                                </div>
+                                
+                            </form>
+
+
+
+                        </div>
+
+                    </div>
+                 
+            </MainLayout>
+
+        
+        </>
+    )
+}
