b14422b8c636decdc281687768fb581804871e3a
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2024-01-15 13:50:44 +0100

    supabase auth login

--- app/auth/page.js
+++ app/auth/page.js
@@ -1,36 +1,45 @@
-"use client"
+"use client";
 
-import {Auth} from '@supabase/auth-ui-react';
-import {ThemeSupa} from '@supabase/auth-ui-shared';
-import Link from 'next/link';
+import { createClientComponentClient} from "@supabase/auth-helpers-nextjs"
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import Link from "next/link";
 
-
-export default function AuthPage(){
-
-        return(
-            <>
-                        <div id='AuthPage' className='w-full min-h-screen bg-white'>
-                            <div className='w-full flex items-center justify-center p-5 border-b-gray-300'>
-                                <Link href="/" className='min-w-[170px]' >
-                                    <img src="/images/SOME.png" width={170} alt="logo" />
-                                </Link>
-                            </div>       
-
-                            <div className='w-full flex items-center justify-center p-5 border-b-gray-300'>
-                                Login / Register
-                            </div>
+export default function AuthPage() {
 
 
-                            <div className='max-w-[400px] mx-auto px-2'>
-                                
-                            </div>
+        // const supabase = createClientComponentClient();
 
-                        </div>            
-            
-            </>
-            
-            
-            
-        )
 
+
+
+
+
+  return (
+    <>
+      <div id="AuthPage" className="w-full min-h-screen bg-white">
+        <div className="w-full flex items-center justify-center p-5 border-b-gray-300">
+          <Link href="/" className="min-w-[170px]">
+            <img src="/images/SOME.png" width={170} alt="logo" />
+          </Link>
+        </div>
+
+        <div className="w-full flex items-center justify-center p-5 border-b-gray-300">
+          Login / Register
+        </div>
+
+        <div className="max-w-[400px] mx-auto px-2">
+          {/* <Auth 
+          
+          onlyThirdPartyProviders 
+          redirectTo={`${window.location.origin}/auth/callback`}
+          supabaseClient={supabase}
+          providers={['google']}
+          appearance={{ theme: ThemeSupa }} 
+          
+          /> */}
+        </div>
+      </div>
+    </>
+  );
 }
