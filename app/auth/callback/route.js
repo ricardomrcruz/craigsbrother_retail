3fec580d4360b42ea6cc3a6d3f153c0bef2a0905
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2024-01-25 00:05:58 +0100

    auth request url typing error

--- app/auth/callback/route.js
+++ app/auth/callback/route.js
@@ -1,19 +1,15 @@
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from 'next/headers'
-import { NextResponse } from 'next/server'
-
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
 
 export async function GET(request) {
-    const requestURL = new URL(request.url)
-    const code = requestUrl.searchParams.get('code')
+  const requestUrl = new URL(request.url);
+  const code = requestUrl.searchParams.get("code");
 
-    if(code) {
-        const supabase = createRouteHandlerClient({ cookies })
-        await supabase.auth.exchangeCodeForSession(code)
-    }
+  if (code) {
+    const supabase = createRouteHandlerClient({ cookies });
+    await supabase.auth.exchangeCodeForSession(code);
+  }
 
-
-    return NextResponse.redirect(requestUrl.origin)
-
+  return NextResponse.redirect(requestUrl.origin);
 }
-
